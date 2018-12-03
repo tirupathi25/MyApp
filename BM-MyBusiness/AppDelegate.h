3d3004de80f3e6999740bb35284c05945e1f031f
tirupathiAmz <tirupathi.mandali@amzurtech.com>
2018-12-03 20:09:00 +0530

    Initial Commit

---
+++ BM-MyBusiness/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BM-MyBusiness
+//
+//  Created by Tirupathi Mandali on 12/3/18.
+//  Copyright © 2018 Tirupathi Mandali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BM-MyBusiness/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BM-MyBusiness
+//
+//  Created by Tirupathi Mandali on 12/3/18.
+//  Copyright © 2018 Tirupathi Mandali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
